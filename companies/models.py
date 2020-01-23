8ab31f55c77c23ed11cb680c1c12a9864b6849b2
Dima Cosmin <cos_d98@yahoo.com>
2020-01-23 20:46:54 +0200

    DSAD

---
+++ companies/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from numeAplicatie.models import Location
+
+
+class Companies(models.Model):
+    name = models.CharField('Company Name', max_length=200)
+    website = models.CharField('Website', max_length=200)
+    location = models.ForeignKey(Location, on_delete=models.ForeignKey)
+
